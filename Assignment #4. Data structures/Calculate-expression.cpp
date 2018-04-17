9c5cc78af6c836658ec0ea658702b5eac3464168
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-04-17 18:28:24 +0300

    Create calculate expression function

---
+++ Assignment #4. Data structures/Calculate-expression.cpp
@@ -0,0 +1,65 @@
+#include <cmath>
+#include <sstream>
+#include <cstring>
+#include "Calculate-expression.h"
+
+char* calculate_expr(Queue* queue) {
+    Stack* stack = new Stack(queue->getSize());
+
+    if (!queue->isEmpty()) {
+        stack->push(queue->dequeue());
+        stack->push(queue->dequeue());
+
+        while (!queue->isEmpty()) {
+            char* symbol = queue->dequeue();
+            if ((int)symbol[0] >= 48 && (int)symbol[0] <= 57) {
+                stack->push(symbol);
+            } else {
+                char* n2 = stack->pop(); char* n1 = stack->pop();
+                stack->push(implement_operator(symbol[0], n1, n2));
+            }
+        }
+    }
+
+    char* result = new char;
+    strcpy(result, stack->pop());
+    delete stack;
+
+    return result;
+}
+
+char* implement_operator(char symbol, char* n1, char* n2) {
+    char* ptr;
+    double first = strtod(n1, &ptr);
+    double second = strtod(n2, &ptr);
+    std::stringstream str;
+
+    switch (symbol) {
+        case '+': {
+            str << first + second;
+            break;
+        }
+        case '-': {
+            str << first - second;
+            break;
+        }
+        case '*': {
+            str << first * second;
+            break;
+        }
+        case '/': {
+            str << first / second;
+            break;
+        }
+        case '^': {
+            str << pow(first, second);
+            break;
+        }
+        default: {
+            str << ' ';
+        }
+    }
+    char* result = new char;
+    strcpy(result, str.str().c_str());
+    return result;
+}
---
+++ Assignment #4. Data structures/Calculate-expression.h
@@ -0,0 +1,11 @@
+#ifndef ASSIGNMENT_4_DATA_STRUCTURES_CALCULATE_EXPRESSION_H
+#define ASSIGNMENT_4_DATA_STRUCTURES_CALCULATE_EXPRESSION_H
+
+
+#include "Queue.h"
+#include "Stack.h"
+
+char* calculate_expr(Queue*);
+char* implement_operator(char, char*, char*);
+
+#endif //ASSIGNMENT_4_DATA_STRUCTURES_CALCULATE_EXPRESSION_H
